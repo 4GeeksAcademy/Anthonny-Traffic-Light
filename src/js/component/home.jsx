d6cb5ef2ef84fb08788858d4c2b0f65c890a454a
PugMau <166648867+PugMau@users.noreply.github.com>
2024-07-10 22:18:08 +0000

    Se termino con exito el proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import ciudadImg from "../../img/ciudad.jpg";
+import "../../styles/index.css"; 
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [activeLight, setActiveLight] = useState(null);
+
+    const handleLightClick = (light) => {
+        setActiveLight(light);
+    };
+
+    const backgroundStyle = {
+        backgroundImage: `url(${ciudadImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        margin: 0,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+    };
+
+    return (
+        <div style={backgroundStyle}>
+            <div className="traffic-light-container">
+                <h1>Traffic Light</h1>
+                <div className="pole"></div>
+                <div className="traffic-light">
+                    <div
+                        className={`light red ${activeLight === "red" ? "on" : ""}`}
+                        onClick={() => handleLightClick("red")}
+                    ></div>
+                    <div
+                        className={`light yellow ${activeLight === "yellow" ? "on" : ""}`}
+                        onClick={() => handleLightClick("yellow")}
+                    ></div>
+                    <div
+                        className={`light green ${activeLight === "green" ? "on" : ""}`}
+                        onClick={() => handleLightClick("green")}
+                    ></div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
